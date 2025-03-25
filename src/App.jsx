d75164f67c95cbb387c7d4a2849f0748d7091412
Emiri Sato <emirisato.1003@gmail.com>
2025-03-25 10:16:41 -0700

    difined variable inside and outside component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const message = 'Coming Soon...'; // This is outside the function definition for App
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const title = ' CTD Swag'; // This is inside the Component before the return
+  const styles = { height: 100, width: 100 };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="coming-soon">
+        <h1>{title}</h1> {/* `title` inserted into heading */}
+        <div style={styles}>
+          <img src={reactLogo} alt="Code The Dream Logo" />
+        </div>
+        <h2>{message}</h2> {/* 'message' inserted into heading*/}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
